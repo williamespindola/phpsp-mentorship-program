becc7015fd46706bbbccb16d4af3e104696bb871
Morkhusz <jose-silvafilho@hotmail.com>
2018-07-23 22:39:05 -0300

    criando testes unitários para create task e testando também a exception

--- to-do-oop/app/Storage/SQLiteAdapter.php
+++ to-do-oop/app/Storage/SQLiteAdapter.php
@@ -59,20 +59,15 @@ class SQLiteAdapter implements TaskStorageAdapterInterface
 
     public function persist(Task $task) : Task
     {
-        try {
-            $insert = "INSERT INTO tasks (title, due, author, description) VALUES (:title, :due, :author, :description)";
-            $stmt = $this->db
-                        ->prepare($insert);
-                        
-            $stmt->bindValue(':title', $task->title());
-            $stmt->bindValue(':due', $task->due()->format('Y-m-d'));
-            $stmt->bindValue(':author', $task->author());
-            $stmt->bindValue(':description', $task->description());
-            $stmt->execute();
+        $insert = "INSERT INTO tasks (title, due, author, description) VALUES (:title, :due, :author, :description)";
+        $stmt = $this->db
+                    ->prepare($insert);
+                    
+        $stmt->bindValue(':title', $task->title());
+        $stmt->bindValue(':due', $task->due()->format('Y-m-d'));
+        $stmt->bindValue(':author', $task->author());
+        $stmt->bindValue(':description', $task->description());
+        $stmt->execute();
 
-            return $task;
-        } catch (\PDOException $exception) {
-            return $exception->getMessage();
-        }
-    }
+        return $task;
 }
--- to-do-oop/tests/CreateTaskTest.php
+++ to-do-oop/tests/CreateTaskTest.php
@@ -4,9 +4,10 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 use DateTime;
-use Todo\Storage\SQLiteAdapter;
+use Todo\Storage\TaskStorageAdapterInterface;
 use Todo\Task;
 use Todo\CreateTask;
+// use Todo\Exception\TaskException;
 
 class CreateTaskTest extends TestCase
 {
@@ -16,15 +17,35 @@ class CreateTaskTest extends TestCase
     public function shouldCreateTaskSuccessfully()
     {
         $task = new Task('lol', new DateTime('01-01-2018'), 'josé filho', 'uma descrição foda');
-        $mock = $this->createMock(CreateTask::class);
 
-        $mock->expects($this->once())
-                        ->method('create')
-                        ->willReturn($task);
+        $storage = $this->createMock(TaskStorageAdapterInterface::class);
+        $storage->expects($this->once())
+                ->method('persist')
+                ->willReturn($task)
+                ->with($this->equalTo($task));
+                
         
-        $createTaskMock = $mock->create($task);
+        $createTask = new CreateTask($storage);
+        $createdTask = $createTask->create($task);
 
-        $this->assertEquals($task, $createTaskMock);
-        $this->assertEquals($task->title(), $createTaskMock->title());
+        $this->assertEquals($task, $createdTask);
+    }
+
+    /**
+     * @test
+     * @expectedException \Todo\Exception\TaskException
+     */
+    public function shouldThrowTaskExceptionWhenAdapterFails()
+    {
+        $task = new Task('lol', new DateTime('01-01-2018'), 'josé filho', 'uma descrição foda');
+
+        $storage = $this->createMock(TaskStorageAdapterInterface::class);
+        $storage->expects($this->once())
+                ->method('persist')
+                ->will($this->throwException(new \Todo\Exception\TaskException('Um erro cabuloso!')))
+                ->with($this->equalTo($task));
+
+        $createTask = new CreateTask($storage);
+        $createTask->create($task);
     }
 }
